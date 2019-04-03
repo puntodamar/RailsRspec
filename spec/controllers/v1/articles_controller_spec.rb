365333d8615a062d821ca7b449d10432a77ee2a1
puntodamar <punto.damar2@gmail.com>
2019-04-03 16:11:29 +0700

    + rspec helper

--- spec/controllers/v1/articles_controller_spec.rb
+++ spec/controllers/v1/articles_controller_spec.rb
@@ -1,19 +1,28 @@
 require 'rails_helper'
+require 'json'
 
 describe V1::ArticlesController, type: :controller do
+    
+    subject { get :index }
+    
     describe "#index" do
         it "should return success" do
-            get :index
+            subject
             expect(response).to have_http_status(:ok)
         end
         
         it "should return proper json" do
-            create_list :article, 2
-            get :index
-            json = JSON.parse(response.body)
-            pp json
-            # data = json[:data]
-            # expect(data.length).to eq(2)
+            articles = create_list :article, 2
+            subject
+            expect(json_data.length).to eq(2)
+            articles.each_with_index do |article, index|
+                expect(json_data[index]["attributes"]).to eq({
+                    "title"     => article.title,
+                    "content"   => article.content,
+                    "slug"      => article.slug
+                })
+            end
+            
         end
     end
 end
